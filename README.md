ce6298af61a2831ed23aa66a629fbe15aab7f3b9
Kevin Heruer <k.heruer@gmail.com>
2021-01-23 01:28:45 +0100

    Implemented rotary encoder

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Edit main.py denon.AVR() line and add IP as string.
 Edit boot.py and edit sta_if.connect() and add wifi name and password.
 
 Interestring stuff is in src/denon/controller.py.
+
+Credits to https://github.com/miketeachman/micropython-rotary for the rotary lib
--- src/main.py
+++ src/main.py
@@ -1,17 +1,36 @@
 import machine
 import utime
 import denon
+import rotary
 
 avr = denon.AVR("")
 
-pin12 = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_UP)
+pin14 = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_UP)
+pin12 = machine.Pin(12, machine.Pin.IN)
+pin13 = machine.Pin(13, machine.Pin.IN)
 
 pwr_pressed = None
 pwr_command_executed = False
 
+r = rotary.RotaryIRQ(pin_num_clk=12, 
+                    pin_num_dt=13, 
+                    min_val=0, 
+                    max_val=100, 
+                    reverse=False, 
+                    range_mode=rotary.RotaryIRQ.RANGE_BOUNDED)
+
+r.set(value=avr.get_volume_level())
+vol_old = r.value()
+
 while True:
+    vol_new = r.value()
+    
+    if vol_old != vol_new:
+        vol_old = vol_new
+        avr.set_volume_level(vol_old)
+
     # Knob is pressed
-    if pin12.value() == 0:
+    if pin14.value() == 0:
         if pwr_pressed is None:
             pwr_pressed = utime.ticks_ms()
 
@@ -24,7 +43,7 @@ while True:
             pwr_command_executed = True
 
     # Knob is not pressed (anymore)
-    if pin12.value() == 1 and pwr_pressed is not None:
+    if pin14.value() == 1 and pwr_pressed is not None:
         pwr_pressed = None
 
         if utime.ticks_diff(utime.ticks_ms(), pwr_pressed) > 2000 and pwr_command_executed:
